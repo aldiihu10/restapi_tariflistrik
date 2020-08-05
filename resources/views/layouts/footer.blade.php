d166ac6a0e89f7a80fcd9c1b0a409f5df62c7bf2
Aldi <afisaldisaldi@gmail.com>
2020-08-05 10:35:35 +0700

    d

--- resources/views/layouts/footer.blade.php
+++
@@ -1,9 +0,0 @@
-<footer class="footer">
-    <div class="container-fluid">
-
-        <p class="copyright pull-right">
-            &copy; <script>document.write(new Date().getFullYear())</script> <a href="#089679360045#">Polinema</a>, PSDKU Politeknik Kediri
-        </p>
-        <!-- © ig:afisaldi_wijanarko -->
-    </div>
-</footer>
